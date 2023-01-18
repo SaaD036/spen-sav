a2a9729697982eaa7f893d4af360a8cee96a6a52
SaaD Ibne Jamal <shaad036@gmail.com>
2023-01-19 01:04:51 +0600

    Add all user api

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const {
     getUsers,
     createUser,
+    getOneUser,
+    updateUser,
+    deleteUser,
 } = require('../../controllers/user');
 
 const {
@@ -15,5 +18,8 @@ const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/', createUserValidtor, validtorHandler, createUser);
+router.get('/:id', getOneUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
