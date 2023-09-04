418db1c96f665a44a7cc2723362a80b1aedf93aa
SaaD Ibne Jamal <shaad036@gmail.com>
2023-09-05 00:45:42 +0600

    Solve cors error

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const cors = require('cors');
 require('dotenv').config();
 
 const router = require('./src/routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
