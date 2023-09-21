99c9d51907f188c62fa815d7aa1e07f30576ddc7
SaaD Ibne Jamal <shaad036@gmail.com>
2023-09-21 23:10:14 +0600

    Add login API and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const userRouter = require('./user');
 const commentRouter = require('./comment');
+const authRouter = require('./auth');
 
 const router = express.Router();
 
+router.use('/auth', authRouter)
 router.use('/user', userRouter);
 router.use('/comment', commentRouter)
 
