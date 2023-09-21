e2f752ccadd1fae11e8b74b438c55b585c95b4ea
SaaD Ibne Jamal <shaad036@gmail.com>
2023-09-22 02:01:53 +0600

    Add auth and admin midleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const authMiddleware = require('../middlewares/auth');
+
 const userRouter = require('./user');
 const commentRouter = require('./comment');
 const authRouter = require('./auth');
@@ -7,7 +9,7 @@ const authRouter = require('./auth');
 const router = express.Router();
 
 router.use('/auth', authRouter)
-router.use('/user', userRouter);
-router.use('/comment', commentRouter)
+router.use('/user', authMiddleware, userRouter);
+router.use('/comment', authMiddleware, commentRouter)
 
 module.exports = router;
